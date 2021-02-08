1a6256b160c3bbd8a0cecb7eefc4f9cc9f285fd9
dbublienko <d.bublienko@gmail.com>
2021-02-08 02:43:44 +0200

    ready proj, fixed bug in routing

---
+++ src/components/pages/Products/Products.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Products() {
+  return (
+    <h1 style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>Products Page</h1>
+  )
+}
+
+export default Products
